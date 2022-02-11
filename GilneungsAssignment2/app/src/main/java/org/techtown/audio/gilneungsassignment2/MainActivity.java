4a0a4665539708bfeddf7f0bc43b9d1502678603
gnleeAndroid <99452338+gnleeAndroid@users.noreply.github.com>
2022-02-11 10:02:29 +0900

    Update MainActivity.java

--- GilneungsAssignment2/app/src/main/java/org/techtown/audio/gilneungsassignment2/MainActivity.java
+++ GilneungsAssignment2/app/src/main/java/org/techtown/audio/gilneungsassignment2/MainActivity.java
@@ -21,18 +21,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = findViewById(R.id.textView);
-        textView2 = findViewById(R.id.textView2);
+        textView2 = findViewById(R.id.textView2); //처음화면은 무조건 빈값인가요 고정값일이유가 없지않을까요
         button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this , SettingActivity.class);
-                startActivity(intent);
+                startActivity(intent); //StartActivity말고 결과값 받는게 여기서 제일 나을것 같습니다.
             }
         });
 
         Intent countriesIntent = getIntent();
         countries = countriesIntent.getStringExtra("countries");
-        textView2.setText(countries);
+        textView2.setText(countries);  //SettingActivity에서 startActivity해서 onCreate 생명주기가 타는건데
+        
     }
 }
